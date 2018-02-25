47853fc27b8e699203fdf0e97a3900805ffcef4c
Reizner Evgeniy <razrfalcon@gmail.com>
2018-02-25 18:50:56 +0200

    gen-table.py writes to file instead of stdout now.

--- update-tables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./gen-table.py elements > site/elements-table.adoc
-./gen-table.py presentation-attributes > site/presentation-attributes-table.adoc
-./gen-table.py attributes > site/attributes-table.adoc
